a2e6b4f3681c84a6cea0d420f9b181b954701c1d
Seb Bains <sebastianbains@gmail.com>
2021-01-28 15:54:38 +0000

    initial files

---
+++ index.js
@@ -0,0 +1,19 @@
+// In this challenge, I want you to create a Dashboard for all our films. Each film should be
+// displayed in our browser, in a box of its own with an image of the film of your choice, similar // to how it would
+// look on Netflix.
+// Please solve this using the film array, as this is an array exercise. Do not hardcode the names,
+// even though it might be tempting!
+
+const films = ['Jaws', 'Aliens', 'Bad Santa', 'Casablanca', 'Ghost', 'Twister', 'legally Blonde', 'Elf', 'Zoolander']
+
+// your code goes here
+
+
+
+
+
+// hint: use a for loop to create the boxes and add information to them.
+
+//level up:
+//1. Are you able to add your own personal rating to each film card too?
+
